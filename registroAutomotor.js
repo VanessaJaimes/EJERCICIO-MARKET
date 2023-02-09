0ba958205d4dad7531cb4ccabf44cc16234fc4f3
Juan Camilo <caamilo9517@gmail.com>
2023-02-09 13:09:51 -0500

    Lógica del registro automotor lista

--- registroAutomotor.js
+++ registroAutomotor.js
@@ -4,18 +4,83 @@ createApp({
     data() {
       return {
         name:'',
-        id:null,
-        dateOfAdmission:null, //Fecha ingreso vehículo
-        failure:null, //Falla del vehículo
-        deadLine:null,
+        id:'',
+        dateOfAdmission:'', //Fecha ingreso vehículo
+        failure:'', //Falla del vehículo
+        deadLine:'',
         inCharge:'', //Encargado de la reparación
-        spareParts:true, //Esta es la variable del radio Button
+
+        spareParts:true, //TODO: esta variable debe enlazarse con el radio button, es la que habilita el modal de repuestos.
+        resumes:[], //Esta es la base de datos con las hojas de vida
 
       }
     },
     methods:{
+      errorAlert(msg){
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: msg,
+        })
+      },
+      successfulAlert(){
+        Swal.fire({
+          position: 'center',
+          icon: 'success',
+          title: 'Ingreso Exitoso',
+          showConfirmButton: false,
+          timer: 2000
+        })
+      },
+      validateData(){
+        //Validación todos los campos llenos
+        const fullFields=(this.name!="" && this.id !='' && this.dateOfAdmission!="" && this.failure!=""
+          && this.deadLine!="" && this.inCharge!="")
+        if(!fullFields){
+          this.errorAlert("Recuerde digitar todos los campos del formulario")
+          return false
+        }
+        //Validación en CC o NIT
+        if(this.id.toString().length<8){
+          this.errorAlert("La identificación o el NIT debe tener al menos 8 dígitos")
+          return false
+        }
+        //Validación Para que la fecha de entrega no sea una fecha antes de la fecha de ingreso.
+        const dateOfAdmissionSeconds=new Date(this.dateOfAdmission).getTime()
+        const deadLineSeconds=new Date(this.deadLine).getTime()
+        if(deadLineSeconds-dateOfAdmissionSeconds<0){
+          this.errorAlert("La fecha de entrega no puede ser antes de la fecha de ingreso del vehículo")
+          return false
+        }
+        return true //Si no entra a ningún if, quiere decir que los datos son correctos.
+      },
       registerResume(){
-        
+        const correctData=this.validateData()
+        if(!correctData){
+          return //Si los datos no son correctos no haga nada.
+        }
+        const resume={
+          name:this.name,
+          id:this.id,
+          dateOfAdmission:this.dateOfAdmission,
+          failure:this.failure,
+          deadLine:this.deadLine,
+          inCharge:this.inCharge, 
+
+        }
+        this.resumes.push(resume)
+        localStorage.setItem("resumes",this.resumes) //Actualizo en el LocalStorage la data.
+        this.successfulAlert()
+
+        console.log(this.resumes)
+
+      }
+    },
+    mount(){
+      this.resumes=localStorage.getItem("resumes")
+      if(resumes===null){
+        this.resumes=[]
       }
     }
+
   }).mount('#app')
