ba8375adcf0295ee02c41ca913199b939a8aab67
Marin28c1997 <96443496+Marin28c1997@users.noreply.github.com>
2023-02-10 11:18:19 -0500

    update garantia.js

--- js/garantia.js
+++ js/garantia.js
@@ -6,17 +6,23 @@ var app = new Vue({
       checkbox3: false,
       dateInput: "",
       result: "",
-      nit:"",
+      ids: JSON.parse(localStorage.getItem("resumes")),
     },
     methods: {
       submitForm() {
+        
+        
         if (!this.checkbox1 || !this.checkbox2 || !this.checkbox3) {
           this.result.innerHTML = swal("","Por favor acabar con el mantenimiento o revisiones para entrega del vehículo","error");
         } else {
-            let date = this.dateInput ? new Date(this.dateInput): new Date();
+            let date = this.ids[0].deadLine ? new Date(this.ids[0].deadLine): new Date();
             let threemonths = new Date(date.getTime() + 90 * 24 * 60 * 1000 * 60);
-            this.result = swal("Entrega del vehículo realizada","Tiene garantía de reparación y respuestos durante 3 meses a partir de la fecha de compra " + date.toLocaleDateString() + " la cual finaliza " + threemonths.toLocaleDateString(),"success");
-        }
-      }
+            let id = this.ids;
+            for (let i = 0; i < this.ids.length; i++) {
+              this.result = swal("Entrega del vehículo realizada","Hola " + id[i].name +" tiene garantía para el vehículo " + id[i].id + " de reparación y respuestos durante 3 meses la cual comienza " + id[i].deadLine + " y finaliza " + threemonths.toLocaleDateString(),"success");
+          } 
+            }
+      },
+
     }
   });
