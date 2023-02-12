a6fa03b344263b53204651044c271172dc289940
Marin28c1997 <96443496+Marin28c1997@users.noreply.github.com>
2023-02-11 20:09:52 -0500

    Update facturaVenta.js

--- js/facturaVenta.js
+++ js/facturaVenta.js
@@ -1,10 +1,14 @@
 var app = new Vue({
-    el: "#app",
-    data: {
-      sale: JSON.parse(localStorage.getItem("productSales",JSON.stringify(this.productsSelected))),
-    },
-    methods: {
+  el: "#app",
+  data: {
+    sale: JSON.parse(
+      localStorage.getItem(
+        "productSales",
+        JSON.stringify(this.productsSelected)
+      )
+    ),
+  },
+  methods: {
 
-    },
-  });
-  
+  },
+});
