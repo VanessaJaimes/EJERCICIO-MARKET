496be2b04b899aa8d244b4ddd4cd7115ebf4db59
Juan Camilo <caamilo9517@gmail.com>
2023-02-12 21:32:29 -0500

    Lógica de Facturación lista, hacen falta las validaciones del formulario

--- js/factura.js
+++ js/factura.js
@@ -7,7 +7,25 @@ const { createApp } = Vue
 
       }
     },
-    mounted(){
+    methods:{
+      addFormat(money){
+        return money.toLocaleString('es-ES',{style:'currency',currency:'COP',maximumFractionDigits: 0})
+      }
+      
+    },
+    computed:{
+      getIva(){
+        return 19
+      },
+      getTotal(){
+        let total=0
+        this.data.vehicleParts.forEach(el => {
+          total+= el.totalPrice
+        })
+        return total
+      }
+    },
+    created(){
         this.data=JSON.parse(localStorage.getItem("data"))
     }
   }).mount('#factura')
